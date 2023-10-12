ea5ca259611db18871fe4c0ef5618920ad5cdba1
Vikx001 <L00179094@atu.ie>
2023-10-12 14:21:20 +0100

    this is the commit in new branch

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 //this is the code to print hello world
 //this is just to check push
+// this is the chnage in the new branch
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
