ab980bf885edf78bb366da82169292b005942f32
Vikx001 <L00179094@atu.ie>
2023-10-12 13:58:24 +0100

    test

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,5 +1,6 @@
 package com.example;
 //this is the code to print hello world
+//this is just to check push
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
